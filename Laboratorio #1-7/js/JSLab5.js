ad5489204d72f1c97a8d299d4d0b5ccf0e0b39b2
Raymundo <A00570654@itesm.mx>
2019-08-27 22:57:28 -0500

    ¡Laboratorio #5 casi terminado!

--- Laboratorio #1-7/js/JSLab5.js
+++ Laboratorio #1-7/js/JSLab5.js
@@ -35,45 +35,80 @@ function nintendo() {
   "";
 }
 function total(){
-  var amountXbox = document.getElementById("AmountXbox");
-  var discountXbox = document.getElementById("DiscountXbox");
+  var amountXbox = document.getElementById("AmountXbox").value;
+  var discountXbox = document.getElementById("DiscountXbox").value;
   var precioXbox = document.getElementById("PrecioXbox");
-  var amountPS = document.getElementById("AmountPS");
-  var discountPS = document.getElementById("DiscountPS");
+  var amountPS = document.getElementById("AmountPS").value;
+  var discountPS = document.getElementById("DiscountPS").value;
   var precioPS = document.getElementById("PrecioPS");
-  var amountNin = document.getElementById("AmountNX");
-  var discountNin = document.getElementById("DiscountNX");
+  var amountNin = document.getElementById("AmountNX").value;
+  var discountNin = document.getElementById("DiscountNX").value;
   var precioNin = document.getElementById("PrecioNX");
   var IVA = 0.16;
   var total = (precioXbox*(1-discountXbox/100)*amountXbox + precioPS*(1-discountPS/100)*amountPS + precioNin*(1-discountNin/100)*amountNin)*(1+IVA);
+  console.log(precioXbox);
+
   document.getElementById("Total").innerHTML = 
     "<h2>" + "Total = $" + total + "</h2>"
   "";
 }
 
-/**
-function checkPassword(form) { 
-  password1 = form.password1.value; 
-  password2 = form.password2.value; 
+
+
+
+
+function evaluarContraseñas() {
+    var contrasenia;
+    var contraseniaConfirm;
+    var tamanio = false;
+    var numeros = false;
+    var letras = false;
+    var caracteres = false;
+    var aceptada = 0;
+    let aceptadaVal = false;
+    
+    document.getElementById("con1").value;
+    
+    contrasenia = document.getElementById("con1").value;
+    contraseniaConfirm = document.getElementById("con2").value;
+
+    
+    if(contrasenia.length==0){
+        tamanio=false;
   
-                // If password not entered 
-                if (password1 == '') 
-                    alert ("Please enter Password"); 
-                      
-                // If confirm password not entered 
-                else if (password2 == '') 
-                    alert ("Please enter confirm password"); 
-                      
-                // If Not same return False.     
-                else if (password1 != password2) { 
-                    alert ("\nPassword did not match: Please try again...") 
-                    return false; 
-                } 
-  
-                // If same return True. 
-                else{ 
-                    alert("Password Match: Welcome to GeeksforGeeks!") 
-                    return true; 
-                } 
-            }
-**/
+    }if(contrasenia.length >= 8){
+        tamanio=true;
+        aceptada++;
+
+    }if(contrasenia.match(/[0-9]/)){
+        numeros=true;
+        aceptada++;
+
+    }if(contrasenia.match(/[a-z]/)){
+        letras=true;
+        aceptada++;
+
+    }if(contrasenia.match(/[!#$%*&.]/)){
+        caracteres = true;
+        aceptada++;
+    }
+    if(aceptada == 4){
+        aceptadaVal = true;
+    }
+    
+    if(contrasenia == contraseniaConfirm && aceptadaVal == false){
+        document.getElementById("feedback").innerHTML = "La contraseña no cumple los requisitos";
+        
+    }else if(contrasenia == contraseniaConfirm && aceptadaVal == true){
+        document.getElementById("feedback").innerHTML = "Contraseña aceptada";
+        
+    }else{
+        document.getElementById("feedback").innerHTML = "Las contraseñas no coinciden";
+    }
+}
+
+function terminar_compra(){
+  document.getElementById("Endgame").innerHTML = 
+    "<h2>" + "¡Compra realizada!" + "</h2>"
+  "";
+}
